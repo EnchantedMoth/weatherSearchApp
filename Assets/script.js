638e6f787562e0fe9fbb2d4edefbcfe4f40156c6
Tim Lewis <timothy.james.lewis.31@gmail.com>
2024-02-20 23:41:45 -0600

    added my own api key to the weatherApiKey variable. everything seems to function properly

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,7 @@
 // Global variables
 var searchHistory = [];
 var weatherApiRootUrl = 'https://api.openweathermap.org';
-var weatherApiKey = 'd91f911bcf2c0f925fb6535547a5ddc9';
+var weatherApiKey = '7229a6e7e0b0a94619e37305a2401bbd';
 
 // DOM element references
 var searchForm = document.querySelector('#search-form');
